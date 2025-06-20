6182d65502ca8f254a351f3c8520c9e67843a5d0
Kate Smith <kate.smith@digital.justice.gov.uk>
2025-06-20 12:07:06 +0100

    doesnt work locally

--- app/addDocument/page.jsx
+++ app/addDocument/page.jsx
@@ -1,21 +1,48 @@
 "use client";
 
-import { useState } from "react";
+import {createRef, useState} from "react";
 import { useRouter } from "next/navigation";
+import * as Papa from "postcss";
 
 export default function AddDocument() {
-    const [name, setName] = useState("");
-    const [content, setContent] = useState("");
+    const fileInput = createRef();
+    const fileName = createRef();
 
     const router = useRouter();
+
+    const changeHandler = (event) => {
+        Papa.parse(event.target.files[0], {
+            header: true,
+            skipEmptyLines: true,
+            complete: function (results) {
+                console.log(results.data)
+            },
+        });
+
+    };
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        if (!name || !content) {
-            alert("Name and content are required.");
-            return;
+        const formData = new FormData();
+        // formData("avatar", fileInput.current.files[0]);
+        // formData.set("fileName", fileName.current.value);
+
+        const reader = new FileReader();
+        const file = fileInput.current.files[0];
+
+
+
+        if (file) {
+            const text = e.target.result
+            console.log("text")
+            console.log(text)
+            reader.readAsText(file);
+            // console.log(reader.read(file));
         }
+
+        console.log("document contents")
+        console.log(FileReader.readAsText(fileInput.current.files[0]))
 
         try {
             const res = await fetch("http://localhost:3000/api/documents", {
@@ -23,7 +50,7 @@ export default function AddDocument() {
                 headers: {
                     "Content-type": "application/json",
                 },
-                body: JSON.stringify({ name, content }),
+                body: formData
             });
 
             if (res.ok) {
@@ -39,19 +66,25 @@ export default function AddDocument() {
     return (
         <form onSubmit={handleSubmit} className="flex flex-col gap-3">
             <input
-                onChange={(e) => setName(e.target.value)}
-                value={name}
+                ref={fileName}
                 className="border border-slate-500 px-8 py-2"
                 type="text"
                 placeholder="Topic Title"
             />
 
             <input
-                onChange={(e) => setContent(e.target.value)}
-                value={content}
+                ref={fileInput}
                 className="border border-slate-500 px-8 py-2"
-                type="text"
-                placeholder="Topic Description"
+                type="file"
+                placeholder="File Here"
+            />
+
+            <input
+                type="file"
+                name="file"
+                accept=".csv"
+                onChange={changeHandler}
+                style={{display: "block", margin: "10px auto"}}
             />
 
             <button
