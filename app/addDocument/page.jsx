15eb660a98ac557df06087255792def0c7f0172e
Kate Smith <kate.smith@digital.justice.gov.uk>
2025-06-20 15:42:42 +0100

    successfully writes to db but db entry is empty

--- app/addDocument/page.jsx
+++ app/addDocument/page.jsx
@@ -1,100 +1,78 @@
 "use client";
 
-import {createRef, useState} from "react";
+import app, {createRef, useState} from "react";
 import { useRouter } from "next/navigation";
-import * as Papa from "postcss";
 
-export default function AddDocument() {
-    const fileInput = createRef();
-    const fileName = createRef();
+async function SubmitForm($router, $fileName, $fileContent) {
 
-    const router = useRouter();
+    try {
+        console.log("0fileName")
+        console.log($fileName)
+        console.log("0fileContent")
+        console.log($fileContent)
 
-    const changeHandler = (event) => {
-        Papa.parse(event.target.files[0], {
-            header: true,
-            skipEmptyLines: true,
-            complete: function (results) {
-                console.log("Papa")
-                console.log(results.data)
+        const res = await fetch("http://localhost:3000/api/documents", {
+            method: "POST",
+            headers: {
+                "Content-type": "application/json",
             },
+            body: JSON.stringify({ $fileName, $fileContent }),
         });
 
-    };
+        if (res.ok) {
+            $router.push("/");
+        } else {
+            throw new Error("Failed to create a document");
+        }
+    } catch (error) {
+        console.log(error);
+    }
+}
+
+export default function AddDocument() {
+    const router = useRouter();
+    const [name, setName] = useState("");
+    const [content, setFile] = useState("");
 
     const handleSubmit = async (e) => {
         e.preventDefault();
 
-        // formData("avatar", fileInput.current.files[0]);
-        // formData.set("fileName", fileName.current.value);
-
-        const form = e.target;
-        const formData = new FormData(form);
-        console.log('formdata')
-        console.log(formData)
-        const formJson = Object.fromEntries(formData.entries());
-        console.log(formJson)
+        console.log("content")
+        console.log(content)
 
         const reader = new FileReader();
-        const file = fileInput.current.files[0]
-        const file2 = formData.get('file');
+        console.log('read')
 
+        reader.onerror = () => {
+            console.log("Error reading the file. Please try again.", "error");
+        };
 
+        reader.onload = () => {
+            console.log('on load function')
+            console.log(reader.result);
+            return SubmitForm(router, name, reader.result);
+        };
 
-        if (file) {
-            const text = e.target.result
-            console.log("text")
-            console.log(text)
-            const data = Papa.parse(text).toString()
-            console.log(data)
-            // reader.readAsText(file);
-            // console.log(reader.read(file));
-        }
+        console.log('read')
+        reader.readAsText(content);
 
-        // console.log("document contents")
-        // console.log(FileReader.readAsText(fileInput.current.files[0]))
-
-        try {
-            const res = await fetch("http://localhost:3000/api/documents", {
-                method: "POST",
-                headers: {
-                    "Content-type": "application/json",
-                },
-                body: formData
-            });
-
-            if (res.ok) {
-                router.push("/");
-            } else {
-                throw new Error("Failed to create a document");
-            }
-        } catch (error) {
-            console.log(error);
-        }
     };
 
     return (
         <form onSubmit={handleSubmit} className="flex flex-col gap-3">
             <input
-                ref={fileName}
+                onChange={(e) => setName(e.target.value)}
+                value={name}
                 className="border border-slate-500 px-8 py-2"
                 type="text"
                 placeholder="Topic Title"
             />
 
             <input
-                ref={fileInput}
+                onChange={(e) => setFile(e.target.files[0])}
                 className="border border-slate-500 px-8 py-2"
                 type="file"
                 placeholder="File Here"
-            />
-
-            <input
-                type="file"
-                name="file"
-                accept=".csv"
-                onChange={changeHandler}
-                style={{display: "block", margin: "10px auto"}}
             />
 
             <button
